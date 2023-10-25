d4e359bfc37eca6903b8b97a288914a0d4b59a55
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-10-25 10:09:50 +0200

    ajout insertion.sql

---
+++ BD/insertion.sql
@@ -0,0 +1,5 @@
+INSERT INTO CATEGORIE VALUES  
+(1, 'Guide orange', 'ff8000'), (2, 'INRS','ffff00'),
+(3, 'INERIS FDRE','00ff00'),(4, 'CEDRE','ff0000'),(5, 'Air Liquide','0000ff'),(8, 'Valeurs toxico','ff00ff'),
+(9, 'BIG','00ffff'),(10, 'Encyclos gaz','8000ff'),(11, 'VSTAF','0080C0'),(12, 'Classeur opérationnel RENZO','80ff80'),
+(13, 'Mmento formules RCH pour FOAD','80ff00'),(14, 'BAO RCH 2021','ff8000'),(15, 'ALHOA','ff0080');
