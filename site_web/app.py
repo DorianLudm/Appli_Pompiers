fd2d6a77ace8edc62a8bb98880ca3bc47d0609f4
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-10-25 16:17:19 +0200

    ajout de la base de données à flask

--- site_web/app.py
+++ site_web/app.py
@@ -1,2 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
+def mkpath(p):
+    return os.path.normpath(os.path.join(os.path.dirname( __file__ ),p))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+mkpath('../BD/DB_pompiers.db'))
+db = SQLAlchemy(app)
