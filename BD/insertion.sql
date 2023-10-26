bc108217c8dd16590e893d411a745fffaf765e6f
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-10-26 08:43:39 +0200

    ajout grade caserne et utilisateur insertion.sql

--- BD/insertion.sql
+++ BD/insertion.sql
@@ -3,3 +3,9 @@ INSERT INTO CATEGORIE VALUES
 (3, 'INERIS FDRE','00ff00'),(4, 'CEDRE','ff0000'),(5, 'Air Liquide','0000ff'),(8, 'Valeurs toxico','ff00ff'),
 (9, 'BIG','00ffff'),(10, 'Encyclos gaz','8000ff'),(11, 'VSTAF','0080C0'),(12, 'Classeur opérationnel RENZO','80ff80'),
 (13, 'Mmento formules RCH pour FOAD','80ff00'),(14, 'BAO RCH 2021','ff8000'),(15, 'ALHOA','ff0080');
+INSERT INTO GRADE VALUES  
+(1, 'Caporal'),(2, 'Sergent'),(3, 'Sergent-Chef'),(4, 'Adjudant'),(5, 'Adjudant-Chef');
+INSERT INTO CASERNE VALUES 
+(1, 'Caserne 1', '1 rue de la caserne');
+INSERT INTO UTILISATEUR VALUES  
+(1, 'LUDMANN', 'Dorianne', 'dorianne','XxDarkSasukexX',1,-1,1)
