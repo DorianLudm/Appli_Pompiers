ff2346bd80a6f2beb096e9460fc7bddb5214af27
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-10-27 10:09:24 +0200

    ajout recherche document recherche_doc.html

--- site_web/views.py
+++ site_web/views.py
@@ -8,5 +8,15 @@ def home():
         tag = dict()
         tag["nom"] = "tag"+str(i)
         print(tag)
-        tags.append(tag)
-    return render_template("recherche_doc.html",tags = tags, active_tags = tags)
+        tags.append(tag) result = []
+    for i in range(4):
+        resultat = dict()
+        resultat["nom"] = "resultat"+str(i)
+        resultat["element"] = []
+        print(resultat)
+        for j in range(4+i):
+            element = dict()
+            element["nom"] = "element"+str(j)
+            resultat["element"].append(element)
+        result.append(resultat)
+    return render_template("recherche_doc.html",tags = tags, active_tags = tags, result = result)
