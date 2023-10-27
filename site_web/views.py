5f6348c02e02c7323e3b4aafa1e72d3b7163c7dc
Sullivaniut <demaret.sullivan@yahoo.com>
2023-10-27 10:13:11 +0200

    Correction chemin css

--- site_web/views.py
+++ site_web/views.py
@@ -8,3 +8,7 @@ def home():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/rechercheDocuments')
+def recherche_document():
+    return render_template('rechercheDocuments.html')
