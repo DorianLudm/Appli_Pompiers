c756cbaadfeeffd59a622cc772961c692f72d1de
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-10-27 09:43:08 +0200

    ajout de la barre de filtre recherche_doc.html

--- site_web/views.py
+++ site_web/views.py
@@ -1,5 +1,12 @@
 from .app import app
+from flask import render_template
 
 @app.route('/')
 def home():
-    return "<h1>Les Pompiers !</h1>"
+    tags = []
+    for i in range(3):
+        tag = dict()
+        tag["nom"] = "tag"+str(i)
+        print(tag)
+        tags.append(tag)
+    return render_template("recherche_doc.html",tags = tags, active_tags = tags)
