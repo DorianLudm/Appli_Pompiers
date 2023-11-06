456283f3cdf4b56f114c888cc328c5d5a63155b0
CharlesMassuard <charles.massuard@gmail.com>
2023-11-06 15:22:19 +0100

    Début page pour rechercher les comptes

--- site_web/views.py
+++ site_web/views.py
@@ -9,6 +9,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/rechercheComptes')
+def recherche_comptes():
+    return render_template('rechercheComptes.html', title='Recherche de comptes')
+
 @app.route('/rechercheDocuments')
 def recherche_document():
     return render_template('rechercheDocuments.html')
