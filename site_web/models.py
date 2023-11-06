79792a3df6bd07426d3bdd46a67a403eaf5a54eb
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2023-11-06 14:27:24 +0100

    ajout de la liason  bd pour tag et type document

--- site_web/models.py
+++ site_web/models.py
@@ -10,3 +10,24 @@ class Utilisateur(db.Model):
     idRole = db.Column(db.Integer)
     idCas = db.Column(db.Integer)
     
+class TypeDocument(db.Model):
+    idType = db.Column(db.Integer, primary_key =True)
+    nomType = db.Column(db.String(100))
+    
+class Document(db.Model):
+    idDoc = db.Column(db.Integer, primary_key =True)
+    nomDoc = db.Column(db.String(100))
+    fichierDoc = db.Column(db.String(100))
+    idType = db.Column(db.Integer, db.ForeignKey('typedocument.idType'))
+    
+class Tag(db.Model):
+    idTag = db.Column(db.Integer, primary_key =True)
+    nomTag = db.Column(db.String(100))
+    niveauProtection = db.Column(db.Integer)
+    couleurTag = db.Column(db.String(100))
+    
+def get_tags():
+    return Tag.query.all()
+
+def get_types():
+    return TypeDocument.query.all()
--- site_web/views.py
+++ site_web/views.py
@@ -1,27 +1,22 @@
-from .app import app
+from .app import app, mkpath
 from flask import render_template
+from .models import get_tags, get_types
+import webbrowser
 
 @app.route('/')
 def home():
-    tags = []
-    for i in range(3):
-        tag = dict()
-        tag["nom"] = "tag"+str(i)
-        print(tag)
-        tags.append(tag) 
     result = []
-    for i in range(4):
+    for i in get_types():
         resultat = dict()
-        resultat["nom"] = "resultat"+str(i)
+        resultat["nomType"] = i.nomType
         resultat["element"] = []
-        print(resultat)
-        for j in range(4+i):
+        for j in range(4):
             element = dict()
-            element["nom"] = "element"+str(j)
+            element["nomDoc"] = "element"+str(j)
             resultat["element"].append(element)
         result.append(resultat)
-    return render_template("recherche_doc.html",tags = tags, active_tags = tags, result = result)
-    return render_template('base.html')
+    webbrowser.open(mkpath('../'))
+    return render_template("recherche_doc.html",tags = get_tags(), active_tags = get_tags(), result = result)
 
 @app.route('/login')
 def login():
