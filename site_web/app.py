5ac9a6a63fdc9c891e8e2dd19ccae0c4662e5bf4
Ludmann Dorian <dorianludm7@gmail.com>
2023-11-07 10:54:23 +0100

    Fix acceuil admin

--- site_web/app.py
+++ site_web/app.py
@@ -1,2 +1,12 @@
+from flask_bootstrap import Bootstrap5
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
+def mkpath(p):
+    return os.path.normpath(os.path.join(os.path.dirname( __file__ ),p))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = ('sqlite:///'+mkpath('../BD/DB_pompiers.db'))
+db = SQLAlchemy(app)
+app.config['BOOTSTRAP_SERVE_LOCAL']=True
+bootstrap = Bootstrap5(app)
--- site_web/views.py
+++ site_web/views.py
@@ -1,5 +1,6 @@
 from .app import app
+from flask import render_template
 
 @app.route('/')
 def home():
-    return "<h1>Les Pompiers !</h1>"
+    return render_template('ajout_compte.html')
