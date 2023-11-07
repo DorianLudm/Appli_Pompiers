60730a2cd7f44c445c25f86451e09d860c8792d9
Ludmann Dorian <dorianludm7@gmail.com>
2023-11-07 11:28:08 +0100

    bump

--- site_web/models.py
+++ site_web/models.py
@@ -9,4 +9,18 @@ class Utilisateur(db.Model):
     idGrade = db.Column(db.Integer)
     idRole = db.Column(db.Integer)
     idCas = db.Column(db.Integer)
-    
+
+class Grade(db.Model):
+    idGrade = db.Column(db.Integer, primary_key =True)
+    nomGrade = db.Column(db.String(10))
+
+def get_grades():
+    return Grade.query.all()
+
+class Caserne(db.Model):
+    idCas = db.Column(db.Integer, primary_key =True)
+    nomCaserne = db.Column(db.String(100))
+    adresseCaserne = db.Column(db.String(100))
+
+def get_caserne():
+    return Caserne.query.all()
--- site_web/views.py
+++ site_web/views.py
@@ -1,9 +1,11 @@
 from .app import app
 from flask import render_template
+# Cet import est un test
+from .models import get_grades, get_caserne
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('ajout_compte.html', grades = get_grades(), casernes = get_caserne())
 
 @app.route('/login')
 def login():
