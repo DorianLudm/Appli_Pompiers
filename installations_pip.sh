8a6d8368dca9166b0ed47f821370296cd74d387f
CharlesMassuard <charles.massuard@gmail.com>
2023-12-19 09:41:17 +0100

    Modification responsive accueil admin

---
+++ installations_pip.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip install python-dotenv
+pip install pyyaml
+pip install flask
+pip install flask-bootstrap
+pip install flask-wtf
+pip install flask-login
+pip install bootstrap-flask
+pip install flask-sqlalchemy
+pip install pysqlite3
+pip install werkzeug==2.2.2
