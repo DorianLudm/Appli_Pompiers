45d0d2cf7edf2b8751c8572dbeb50257bc0b8026
Leopold-ch <leopold.chidlovsky@etu.univ-orleans.fr>
2024-02-21 12:17:40 +0100

    correction des requirements

---
+++ installations_pip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+pip install python-dotenv
+pip install pyyaml
+pip install flask
+pip install flask-wtf
+pip install flask-login
+pip install flask-sqlalchemy
+pip install pysqlite3
+pip install werkzeug==2.2.2
+pip install bootstrap-flask
+pip install pdfminer.six
+pip install nltk
+pip install unidecode
