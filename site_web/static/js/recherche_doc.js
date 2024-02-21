6974517e702864346220c9632fbeb8c740b4f161
CharlesMassuard <charles.massuard@gmail.com>
2024-02-21 15:00:51 +0100

    fix tag en double

--- site_web/static/js/recherche_doc.js
+++ site_web/static/js/recherche_doc.js
@@ -12,19 +12,27 @@ window.onload = function() {
             return response.json(); // Change this line
         })
         .then(function(tagObject) {
-            // Créez un nouvel élément input
+            // Obtenez la div active_tag
+            var activeTagDiv = document.getElementsByClassName('active_tag')[0];
+        
+            // Parcourez tous les éléments input dans la div active_tag
+            var inputs = activeTagDiv.getElementsByTagName('input');
+            for (var i = 0; i < inputs.length; i++) {
+                if (inputs[i].value === tagObject.nomTag) {
+                    // Si le tag sélectionné est déjà affiché, ne faites rien et retournez
+                    return;
+                }
+            }
+        
+            // Si le tag sélectionné n'est pas déjà affiché, créez un nouvel élément input
             var newInput = document.createElement("input");
             newInput.setAttribute("type", "submit");
             newInput.setAttribute("name", "retirer_filtre");
-            newInput.setAttribute("value", tagObject.nomTag); // Change this line
-            newInput.style.backgroundColor = "#" + tagObject.couleurTag; // Add this line
+            newInput.setAttribute("value", tagObject.nomTag);
+            newInput.style.backgroundColor = "#" + tagObject.couleurTag;
         
             // Ajoutez le nouvel élément à la div active_tag
-            var activeTagDiv = document.getElementsByClassName('active_tag')[0];
             activeTagDiv.appendChild(newInput);
-        
-            // Stockez le tag sélectionné dans le localStorage
-            localStorage.setItem('selectedTag', selectedTag);
         });
     });
 }
